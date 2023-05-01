54cf22a2711ae859d1805c5bd7fb76abfe9913af
mikiyoshi <mikiyoshi.kokura@gmail.com>
2023-04-30 23:51:41 -0400

    Push .env

---
+++ src/pages/Test/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Test({ title }) {
+  return (
+    <div className="overflow-scroll">
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+      <h4>First heading</h4>
+      <p>...</p>
+    </div>
+  );
+}
+
+export default Test;
